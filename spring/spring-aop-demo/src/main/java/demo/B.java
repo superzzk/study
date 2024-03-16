7bf004ee9bda5295f6c372e50aa52b3c28ea143d
superzzk <superzzk@qq.com>
2024-03-16 15:40:36 +0800

    update

--- spring/spring-aop-demo/src/main/java/demo/B.java
+++
@@ -1,9 +0,0 @@
-package demo;
-
-/**
- * @author zhangzhongkun
- * @since 2019-12-19 11:29
- **/
-public interface B {
-    void m();
-}
--- spring/spring-aop-demo/src/main/java/demo/ThrowsAdvisor.java
+++
@@ -1,9 +0,0 @@
-package demo;
-
-import org.springframework.aop.ThrowsAdvice;
-
-public class ThrowsAdvisor implements ThrowsAdvice {
-    public void afterThrowing(Exception ex){
-        System.out.println("additional concern if exception occurs");
-    }
-}
